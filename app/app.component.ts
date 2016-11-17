21538b3c911afc69a038b295840820aed38af479
Justen <justenbarget@gmail.com>
2016-11-17 20:35:57 +0000

    adding initial file set up

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'main-app',
+  template: '<div class="main-app">Hello There</div>'
+})
+
+export class AppComponent {
+  constructor() {}
+}
